c95fce6b15e1014f6d440b178dcb176606defdc3
Gian Carlo Catalan <166023368+jiyancarloo@users.noreply.github.com>
2026-03-19 16:28:30 +0800

    Update News.tsx

--- src/app/sections/newsSection/News.tsx
+++ src/app/sections/newsSection/News.tsx
@@ -1,10 +1,12 @@
 import Section from "@/src/components/ui/Theme/ThemeWrapper";
-
+import Container from "@/src/components/layout/Container";
 export default function NewsSection() {
   return (
     <>
-      <Section theme="light">
-        <div>NEWS SECTION</div>
+      <Section theme="light" className="min-h-screen">
+        <Container>
+          <div className="py-40">NEWS SECTION</div>
+        </Container>
       </Section>
     </>
   );
